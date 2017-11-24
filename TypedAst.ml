0edfe046200e9b7cbb71b8f46b4072bfbb39d777
Nchlt <nchlltaf@gmail.com>
2017-11-24 11:19:07 +0100

    constructeur mk_arrayaccess

--- TypedAst.ml
+++ TypedAst.ml
@@ -2,30 +2,27 @@
 
 module Symb_Tbl = SourceAst.Symb_Tbl
 
-open SourceAst
-(*
-type function_info    = SourceAst.function_info
-type program          = SourceAst.program
-type identifier_kind  = SourceAst.identifier_kind
-type identifier_info  = SourceAst.identifier_info
-type typ              = SourceAst.typ
-type block            = SourceAst.block
-type instruction      = SourceAst.instruction (*x*)
-type expression       = SourceAst.expression  (*x*)
-type call             = SourceAst.call        (*x*)
-type literal          = SourceAst.literal
-type location         = SourceAst.location
-type binop            = SourceAst.binop
-*)
+module S = SourceAst
 
-type typed_expression =  typ           expression  annotated_element
-type typed_location   =  typ           location    annotated_element
-type typed_call       = (typ option)   call        annotated_element
+type function_info    = S.function_info
+type program          = S.program
+type identifier_kind  = S.identifier_kind
+type identifier_info  = S.identifier_info
+type typ              = S.typ
+type call             = S.call
+type literal          = S.literal
+type binop            = S.binop
+
+
+type typed_expression =  typ     *      S.expression * annotated_element
+type typed_location   =  typ     *      S.location   * annotated_element
+type typed_call       = (typ option) *  S.call       * annotated_element
 
 and 'a 'e annotated_element = { annot: 'a  ;
                                 elt  :  'e }
+and block = instruction list
 
-and instructions =
+and instruction =
   | Set   of typed_location   * typed_expression    (* Affectation       *)
   | While of typed_expression * block               (* Boucle            *)
   | If    of typed_expression * block * block       (* Branchement       *)
@@ -39,4 +36,18 @@ and expression =
   | FunCall   of typed_call                                   (* Appel de fonction   *)
   | NewArray  of typed_expression * typ                       (* Création de tableau *)
 
+and location =
+  | Identifier  of string   (* Variable en mémoire *)
+  | ArrayAccess of typed_a_access (* Case d'un tableau   *)
+
 and a_access = typed_expression * typed_expression
+
+(* Les constructeurs *)
+
+let mk_arrayaccess te1 te2 =
+  let (type_te1, _, _) = te1 in
+  let (type_te2, _, _) = te2 in
+  match type_te1, type_te2 with
+  | TypeArray(t), TypeInteger ->
+    { annot = t; elt: ArrayAccess(te1, te2) }
+  | _ -> failwith "Found ArrayAccess violation"
